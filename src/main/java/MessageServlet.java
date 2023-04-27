28c81fcd8c27fe9ff5e71959f0020bcbcb10462c
sosuisen <hidekazu.kubota@gmail.com>
2023-04-28 01:41:05 +0900

    fix: use var

--- src/main/java/MessageServlet.java
+++ src/main/java/MessageServlet.java
@@ -7,7 +7,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import model.MessageBean;
 
@@ -19,7 +18,7 @@ public class MessageServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		HttpSession session = request.getSession();
+		var session = request.getSession();
 
 		if (session.getAttribute("history") == null) {
 			session.setAttribute("history", new ArrayList<MessageBean>());
